d771a63ae0cf0b0dffc4e73f726089ea33120b4b
Kody <34889710+Myraas@users.noreply.github.com>
2025-04-01 02:00:38 -0700

    Add files via upload

---
+++ ea_mdm_enrollment.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ea_mdm_enrollment
+# Purpose: Check MDM enrollment status
+# Global Custom Field: Type: Text, Label: ea_mdm_enrollment, Name: eamdmenrollment, Scope: Device
+# Run "bash ea_mdm_enrollment.sh -debug" to view results in a local terminal
+
+DEBUG_MODE=false
+if [[ "$1" == "-debug" ]]; then
+    DEBUG_MODE=true
+fi
+
+# Get MDM enrollment information directly from profiles command
+result=$(profiles status -type enrollment)
+
+if $DEBUG_MODE; then
+    echo "DEBUG OUTPUT: eamdmenrollment value:"
+    echo "$result"
+    exit 0
+fi
+
+/Applications/NinjaRMMAgent/programdata/ninjarmm-cli set eamdmenrollment "$result"
