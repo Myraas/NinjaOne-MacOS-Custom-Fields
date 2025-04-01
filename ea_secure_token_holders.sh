0f3cd2c5a9da00505028a9bdd82dc5d308a4b40d
Kody <34889710+Myraas@users.noreply.github.com>
2025-04-01 01:37:59 -0700

    Update ea_secure_token_holders.sh

--- ea_secure_token_holders.sh
+++ ea_secure_token_holders.sh
@@ -1,8 +1,8 @@
-#!/bin/zsh
+#!/bin/bash
 # ea_secure_token_holders
 # Purpose: Report all user accounts who have a secure token
 # Global Custom Field: Type: Text, Label: ea_secure_token_holders, Name: easecuretokenholders, Scope: Device
-# Run "zsh ea_secure_token_holders.sh -debug" to view results in a local terminal
+# Run "bash ea_secure_token_holders.sh -debug" to view results in a local terminal
 
 DEBUG_MODE=false
 if [[ "$1" == "-debug" ]]; then
@@ -23,42 +23,49 @@ if [[ "$fsType" != *APFS* ]]; then
     exit 0
 fi
 
-secureTokenAdmins=()
-secureTokenUsers=()
+declare -a secureTokenAdmins
+declare -a secureTokenUsers
 
-for uuid in ${$(/usr/sbin/diskutil apfs listUsers / | /usr/bin/awk '/\+\-\-/ {print $2}')}; do
+uuids=$(/usr/sbin/diskutil apfs listUsers / | /usr/bin/awk '/\+\-\-/ {print $2}')
+
+for uuid in $uuids; do
     username="$(/usr/bin/dscl . -search /Users GeneratedUID ${uuid} | /usr/bin/awk 'NR==1{print $1}')"
     
     if /usr/sbin/dseditgroup -o checkmember -m "$username" admin &>/dev/null; then
-        secureTokenAdmins+=($username)
+        secureTokenAdmins+=("$username")
     else
-        secureTokenUsers+=($username)
+        secureTokenUsers+=("$username")
     fi
 done
 
-if [[ -z ${secureTokenAdmins[@]} ]]; then
-    stList="$(echo "Admins: None")"
+tmpfile=$(mktemp)
+
+if [ ${#secureTokenAdmins[@]} -eq 0 ] && [ ${#secureTokenUsers[@]} -eq 0 ]; then
+    echo "No Secure Token Users" > "$tmpfile"
 else
-    stList="$(echo "Admins: ${secureTokenAdmins[1]}")"
-    
-    for user in ${secureTokenAdmins[@]:1}; do
-        stList+=", $user"
-    done
+    if [ ${#secureTokenAdmins[@]} -eq 0 ]; then
+        echo "Admins: None" > "$tmpfile"
+    else
+        admin_list="Admins: ${secureTokenAdmins[0]}"
+        for ((i=1; i<${#secureTokenAdmins[@]}; i++)); do
+            admin_list+=", ${secureTokenAdmins[$i]}"
+        done
+        echo "$admin_list" > "$tmpfile"
+    fi
+
+    if [ ${#secureTokenUsers[@]} -eq 0 ]; then
+        echo "Non-Admins: None" >> "$tmpfile"
+    else
+        non_admin_list="Non-Admins: ${secureTokenUsers[0]}"
+        for ((i=1; i<${#secureTokenUsers[@]}; i++)); do
+            non_admin_list+=", ${secureTokenUsers[$i]}"
+        done
+        echo "$non_admin_list" >> "$tmpfile"
+    fi
 fi
 
-if [[ -z ${secureTokenAdmins[@]} ]] && [[ -z ${secureTokenUsers[@]} ]]; then
-    stList="$(echo "No Secure Token Users")"
-elif [[ -z ${secureTokenUsers[@]} ]]; then
-    stList+="\n$(echo "Non-Admins: None")"
-else
-    stList+="\n$(echo "Non-Admins: ${secureTokenUsers[1]}")"
-    
-    for user in ${secureTokenUsers[@]:1}; do
-        stList+=", $user"
-    done
-fi
-
-result="$stList"
+result=$(cat "$tmpfile")
+rm "$tmpfile"
 
 if $DEBUG_MODE; then
     echo "DEBUG OUTPUT: easecuretokenholders value:"
